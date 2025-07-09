ba7ef9b859a3c8f62fa273b44e23c5a5bb33b3b6
Sky <skydebreuil@gmail.com>
2025-07-08 19:20:11 -0500

    Add persistence.nvim session helpers

--- dot_config/nvim/lua/plugins/persistence.lua
+++ dot_config/nvim/lua/plugins/persistence.lua
@@ -1,5 +1,31 @@
 return {
   "folke/persistence.nvim",
   event = "BufReadPre",
-  opts = {},
+  opts = {
+    -- always save sessions even when no buffers are open
+    need = 0,
+  },
+  keys = {
+    {
+      "<leader>qs",
+      function()
+        require("persistence").load()
+      end,
+      desc = "Restore session",
+    },
+    {
+      "<leader>ql",
+      function()
+        require("persistence").load({ last = true })
+      end,
+      desc = "Restore last session",
+    },
+    {
+      "<leader>qd",
+      function()
+        require("persistence").stop()
+      end,
+      desc = "Don't save this session",
+    },
+  },
 }
