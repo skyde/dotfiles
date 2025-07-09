22968c8d7c2b970d853369d7ec792755215b13e6
Sky <skydebreuil@gmail.com>
2025-07-09 10:55:36 -0500

    Remove chezmoi auto update

--- bin/chezmoi-auto-update.ps1
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env pwsh
-if (Get-Command chezmoi -ErrorAction SilentlyContinue) {
-    $status = chezmoi git status --porcelain
-    if (-not $status) {
-        chezmoi update --init | Out-Null
-    } else {
-        Write-Verbose "chezmoi has local changes, skipping auto update." -Verbose
-    }
-}
