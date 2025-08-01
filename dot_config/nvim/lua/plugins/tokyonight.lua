018e708938aef5bf416d86d327e165c3824b59c8
Sky <skydebreuil@gmail.com>
2025-08-01 17:04:34 -0500

    nvim: use default diff colors

--- dot_config/nvim/lua/plugins/tokyonight.lua
+++ dot_config/nvim/lua/plugins/tokyonight.lua
@@ -17,10 +17,7 @@ return {
     config = function(_, opts)
       require("tokyonight").setup(opts)
       vim.cmd.colorscheme("tokyonight")
-      vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#203227" })
-      vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#37222c" })
-      vim.api.nvim_set_hl(0, "DiffChange", { bg = "#1d2437" })
-      vim.api.nvim_set_hl(0, "DiffText", { bg = "#1d2437", bold = true })
+      -- use colorscheme defaults for diff highlighting
     end,
   },
 }
