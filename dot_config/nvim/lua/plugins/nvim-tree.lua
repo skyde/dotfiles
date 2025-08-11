6b1f322ac1a7e1c2d350c2905471e133c0253565
Sky <skydebreuil@gmail.com>
2025-08-11 15:37:35 -0500

    feat(nvim): map <leader>fe to toggle NvimTree

---
+++ dot_config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  keys = {
+    {
+      "<leader>fe",
+      "<cmd>NvimTreeToggle<CR>",
+      desc = "Toggle file explorer",
+    },
+  },
+  opts = {},
+}
