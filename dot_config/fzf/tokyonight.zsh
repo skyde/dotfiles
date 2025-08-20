ff3e37472756b64a308fccc7504d578f11039cef
Free Debreuil <freedebreuil@gmail.com>
2025-08-20 01:57:58 -0500

    update find in vs code and cmd line programs

--- dot_config/fzf/tokyonight.zsh
+++ dot_config/fzf/tokyonight.zsh
@@ -1,14 +1,19 @@
-# FZF look & feel similar to LazyVim's dropdown
+# TokyoNight-ish fzf with LazyVim/Telescope-like dropdown UX
 export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS
   --layout=reverse
-  --border=rounded
   --height=60%
-  --info=inline
+  --border=rounded
   --margin=1,3
+  --info=inline
   --no-separator
+  --ansi
   --pointer='❯'
   --marker='✓'
-  --ansi
+  --tiebreak=index
   --bind=ctrl-j:down,ctrl-k:up,alt-j:down,alt-k:up,ctrl-u:half-page-up,ctrl-d:half-page-down,alt-p:toggle-preview
-  --color=fg:#c0caf5,bg:#1a1b26,hl:#7aa2f7,fg+:#c0caf5,bg+:#24283b,hl+:#7aa2f7,info:#7aa2f7,prompt:#7dcfff,pointer:#ff9e64,marker:#9ece6a,spinner:#bb9af7,header:#565f89,border:#3b4261
+  # left-list highlight colors (query match)
+  --color=fg:#c0caf5,bg:#1a1b26,fg+:#c0caf5,bg+:#24283b
+  --color=hl:#ff9e64,hl+:#ff9e64
+  --color=info:#7aa2f7,prompt:#7dcfff,pointer:#ff9e64,marker:#9ece6a,spinner:#bb9af7,header:#565f89,border:#3b4261
 "
+
