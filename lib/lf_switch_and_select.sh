3c70e28c6485d33e731502f8be4df99f9d2093bb
Sky <skydebreuil@gmail.com>
2025-08-21 09:00:36 -0500

    Removed logs

--- lib/lf_switch_and_select.sh
+++ lib/lf_switch_and_select.sh
@@ -4,11 +4,11 @@
 # Usage: Called from VS Code tasks with VSC_CWD and VSC_FILE environment variables
 
 # Navigate LF to the current directory
-echo "send cd \"${VSC_CWD:-${WORKSPACE:-$PWD}}\""
+# echo "send cd \"${VSC_CWD:-${WORKSPACE:-$PWD}}\""
 lf -remote "send cd \"${VSC_CWD:-${WORKSPACE:-$PWD}}\""
 
 # Select the current file (basename only since we're already in the directory)
 if [ -n "$VSC_FILE" ]; then
-    echo "send select \"$(basename "$VSC_FILE")\""
+    # echo "send select \"$(basename "$VSC_FILE")\""
     lf -remote "send select \"$(basename "$VSC_FILE")\""
 fi
