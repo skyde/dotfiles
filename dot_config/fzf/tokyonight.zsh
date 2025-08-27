07577ebb16cd61497feba0c9c3b561ebde954b3b
Free Debreuil <freedebreuil@gmail.com>
2025-08-27 01:25:44 -0500

    install tokyo night theme for bat

--- dot_config/fzf/tokyonight.zsh
+++ dot_config/fzf/tokyonight.zsh
@@ -1,4 +1,5 @@
 # TokyoNight-ish fzf with LazyVim/Telescope-like dropdown UX
+export BAT_THEME="tokyonight_night"
 export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS
   --layout=reverse
   --height=60%
@@ -14,6 +15,5 @@ export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS
   # left-list highlight colors (query match)
   --color=fg:#c0caf5,bg:#1a1b26,fg+:#c0caf5,bg+:#24283b
   --color=hl:#ff9e64,hl+:#ff9e64
-  --color=info:#7aa2f7,prompt:#7dcfff,pointer:#ff9e64,marker:#9ece6a,spinner:#bb9af7,header:#565f89,border:#3b4261
-"
+  --color=info:#7aa2f7,prompt:#7dcfff,pointer:#ff9e64,marker:#9ece6a,spinner:#bb9af7,header:#565f89,border:#3b4261,preview-fg:#c0caf5,preview-bg:#24283b
 
