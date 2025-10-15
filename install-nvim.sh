cb8796d9011a11cc822800aece273f4c4ad042c7
Sky <skydebreuil@gmail.com>
2025-10-15 15:57:47 -0400

    Add script to install latest Neovim

---
+++ install-nvim.sh
@@ -0,0 +1,4 @@
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
+chmod u+x nvim-linux-x86_64.appimage
+mkdir -p ~/.local/bin
+mv nvim-linux-x86_64.appimage ~/.local/bin/nvim
