55dad83492cd736703f1cbfb0c8c9474bb82051a
Sky <skydebreuil@gmail.com>
2026-07-28 20:00:08 -0400

    Revert "Fixed flicker"
    This reverts commit e4954f336b955ab4461a823d7556767b2a333409.

--- common/.config/yazi/init.lua
+++ common/.config/yazi/init.lua
@@ -1,7 +1,4 @@
 -- require("git"):setup()
--- Preload the bounded text previewer so Yazi knows its peek function is
--- synchronous before the first cursor move.
-require("sync-text-preview")
 require("relative-line-numbers"):setup()
 
 require("zoxide"):setup({
